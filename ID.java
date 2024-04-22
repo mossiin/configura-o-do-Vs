05b7dbd290b653355c3ae2a5c7c74a1db5212b95
Lucas <zdarkheadbr@gmail.com>
2024-04-22 09:42:31 -0300

    segundocommit

---
+++ ID.java
@@ -0,0 +1,19 @@
+public class ID {
+    
+     public   String nome = "";
+     public   int idade = 0;
+     public   String email = "";
+       
+   // Setter para o nome idea do GPT-3.5
+   public void setNome(String nome) {
+    this.nome = nome;
+}
+   // Setter para iDADE
+   public void setIdade(int idade) {
+    this.idade = idade;
+   }
+   //Setter para email
+   public void setEmail(String email) {
+    this.email = email;
+   }
+}
---
+++ Show.java
@@ -0,0 +1,11 @@
+public class Show {
+
+    int id;
+    String nomeShow;
+    double tamanho;
+    float data;
+
+    public void Espetaculo (String nomeShow){
+        this.nomeShow = nomeShow ;
+    }
+}
